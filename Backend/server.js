db94da52c76c1e0e71714f287ba0c0896d8c9f72
saikiranyad <saisaikiran7890@gmail.com>
2024-10-11 06:36:39 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const TodoModel = require('./Models/Todo');
-const dotenv = require('dotenv').config()
+
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-mongoose.connect(process.env.MONGOURI).then(()=>console.log("db is connected")).catch(err=>console.log(err))
+mongoose.connect('mongodb+srv://sai:sai1234@cluster0.xhbqw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("db is connected")).catch(err=>console.log(err))
 
 
 app.get('/get',(req,res)=>{
